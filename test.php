f7a640b121ebca1fbe1dc0a7da7df45ae82ab0e3
SalmaaaTareeek <salmaaatareeek97@gmail.com>
2020-10-24 00:36:43 +0200

    Testing the application by FORM

--- test.php
+++ test.php
@@ -1,42 +1,3 @@
-<?php
-
-include ('index.php');
-
-$array_of_products =array();
-$array_of_currency = array();
-$combination_product = 0;
-$combination_currency = 0;
-
-
-	if(isset($_POST['submit']))
-	{
-		// echo $_POST['currency'] . '<br />';
-		
-
-		$combination_product = $_POST['products_form'];
-		$combination_currency = $_POST['currency_form'];
-		$string1 = $combination_product[0];
-	    $string2 = $combination_currency;
-	    $array_of_products = explode(",",$string1);
-	    array_push($array_of_currency, $string2);
-	}
-
-$x = new Product($array_of_products,$array_of_currency);
-echo $x->Compare($array_of_products,$array_of_currency);
-echo $x->Discount_of_Shoes($array_of_products,$array_of_currency);
-echo $x->Discount_of_Tshirts($array_of_products,$array_of_currency);
-echo $x->Total($array_of_products,$array_of_currency);
-
-
-
-
-
-
-?> 
-
-
-
-
 
 <!DOCTYPE html>
 <html>
@@ -54,6 +15,36 @@ echo $x->Total($array_of_products,$array_of_currency);
 				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
 			</div>
 		</form>
+	</section>
+	<section class="container grey-text">
+		<h4 class="center">Your Bill</h4>
+
+	<form class="white" action="test.php" method="POST">
+			<?php
+			include ('index.php');
+			$array_of_products =array();
+			$array_of_currency = array();
+			$combination_product = 0;
+			$combination_currency = 0;
+			if(isset($_POST['submit']))
+			{
+				$combination_product = $_POST['products_form'];
+				$combination_currency = $_POST['currency_form'];
+				$string1 = $combination_product[0];
+	    		$string2 = $combination_currency;
+	    		$array_of_products = explode(",",$string1);
+	    		array_push($array_of_currency, $string2);
+			}
+
+		$x = new Product($array_of_products,$array_of_currency);
+		echo $x->Compare($array_of_products,$array_of_currency);
+		echo $x->Discount_of_Shoes($array_of_products,$array_of_currency);
+		echo $x->Discount_of_Tshirts($array_of_products,$array_of_currency);
+		echo $x->Total($array_of_products,$array_of_currency);
+?> 
+		</form>
+	
+		
 	</section>
 
 
